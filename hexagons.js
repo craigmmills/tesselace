371126e40483049fe7e56311a190ca6bf85bb03d
Craig Mills <craig.mills@unep-wcmc.org>
2011-12-29 00:41:09 +0000

    removed json library

--- hexagons.js
+++ hexagons.js
@@ -1,111 +1,104 @@
-
-
 jQuery.noConflict();
-var $j = jQuery;			
+var $j = jQuery;
 
 $j(document).ready(function() {
+	"use strict";
 	var map = new L.Map('map');
 
 	var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/22677/256/{z}/{x}/{y}.png',
 		cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-		cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttribution});
+		cloudmade = new L.TileLayer(cloudmadeUrl, {
+		maxZoom: 18,
+		attribution: cloudmadeAttribution
+	});
 
-	map.setView(new L.LatLng(0, 0), 3).addLayer(cloudmade);
-	
-	
+	map.setView(new L.LatLng(0, 0), 6).addLayer(cloudmade);
+
 	var geojsonLayer = new L.GeoJSON();
 	map.addLayer(geojsonLayer);
-	
-	geojsonLayer.on("featureparse", function (e){
-	    if (e.properties && e.properties.style && e.layer.setStyle){
-	        // The setStyle method isn't available for Points. More on that below ...
-	        e.layer.setStyle(e.properties.style);
-	    }
+
+	geojsonLayer.on("featureparse", function(e) {
+		if (e.properties && e.properties.style && e.layer.setStyle) {
+			// The setStyle method isn't available for Points. More on that below ...
+			e.layer.setStyle(e.properties.style);
+		}
 	});
 	geojsonLayer.addGeoJSON(test_hex());
-		
+
 });
 
 //coordinates of each hexagon
-function hexagon (length, x_start, y_start){
-	
-	var hex=[[length + x_start, y_start],
-	[((length/2) + x_start), (length * Math.sqrt(length/2)) + y_start],
-	[((-length/2) + x_start), (length * Math.sqrt(length/2)) + y_start],
-	[(-length + x_start), y_start],
-	[((-length/2) + x_start), (-length * Math.sqrt(length/2)) + y_start],
-	[(length/2) + x_start, (-length * Math.sqrt(length/2)) + y_start],
-	[length + x_start, y_start]
-	]; 
+function hexagon(length, x_start, y_start) {
+	"use strict";
+	var hex = [
+		[length + x_start, y_start],
+		[((length / 2) + x_start), (length * Math.sqrt(length / 2)) + y_start],
+		[((-length / 2) + x_start), (length * Math.sqrt(length / 2)) + y_start],
+		[(-length + x_start), y_start],
+		[((-length / 2) + x_start), (-length * Math.sqrt(length / 2)) + y_start],
+		[(length / 2) + x_start, (-length * Math.sqrt(length / 2)) + y_start],
+		[length + x_start, y_start]];
 	return hex;
 
 }
 
-
 //origin is starting coordinates [x,y], length in degs for now, bbox: [xmin, xmax, ymin, ymax]
-function generate_multihex (length, bbox){
+function generate_multihex(length, bbox) {
+	"use strict";
+	var current_pos = [];
+	//container to hold the full file
+	var geojson;
 
-	var current_pos=[];
-  //container to hold the full file
-  var geojson;
-
-  //array to hold the features
-  var features = [];
-  var origin = [bbox[0], bbox[3]]; 
-  var up = false; //hack to align the hexagons y location
-  	
+	//array to hold the features
+	var features = [];
+	var origin = [bbox[0], bbox[3]];
+	var up = false; //hack to align the hexagons y location
 	current_pos = origin;
-	var cur_pos_ar=[];
-	
-  while (current_pos[0] < bbox[1]){
-    while (current_pos[1] > bbox[2]){
-			features.push(hexagon(length, current_pos[0], current_pos[1]));
-	    current_pos = [current_pos[0], current_pos[1] - (length * 2)]; 
-    }
+	var cur_pos_ar = [];
 
-    //shift the hexagon creation to the next column with a shift in the y to nest them
-    if (up){
-      current_pos = [current_pos[0] + (length*1.5), bbox[3]];
-      up = false;
+	while (current_pos[0] < bbox[1]) {
+		while (current_pos[1] > bbox[2]) {
+			features.push(hexagon(length, current_pos[0], current_pos[1]));
+			current_pos = [current_pos[0], current_pos[1] - ((length * Math.sqrt(length / 2)) * 2)];
 		}
-    else{
-      current_pos = [current_pos[0] + (length*1.5), bbox[3] + (length * Math.sqrt(length/2))];
-      up = true;
-    }
-  }
-    
-	
-  geojson = { "type": "FeatureCollection",
-	  "features": [
-	    { 
-				"type": "Feature",
-	      "geometry": {"type": "MultiPolygon", "coordinates": [features]},
-				"properties": {"prop1": 0.0, 
+
+		//shift the hexagon creation to the next column with a shift in the y to nest them
+		if (up) {
+			current_pos = [current_pos[0] + (length * 1.5), bbox[3]];
+			up = false;
+		} else {
+			current_pos = [current_pos[0] + (length * 1.5), bbox[3] + (length * Math.sqrt(length / 2))];
+			up = true;
+		}
+	}
+
+	geojson = {
+		"type": "FeatureCollection",
+		"features": [{
+			"type": "Feature",
+			"geometry": {
+				"type": "MultiPolygon",
+				"coordinates": [features]
+			},
+			"properties": {
+				"prop1": 0.0,
 				"style": {
-				            "color": "#DA5B38",
-				            "weight": 0.1,
-				            "opacity": 0.8
-				        }}
-			}]
-	   };
-	
-	
-	
+					"color": "#DA5B38",
+					"weight": 0.1,
+					"opacity": 1
+				}
+			}
+		}]
+	};
+
 	return geojson;
 
 }
 
 
 
-
-function test_hex(){
-
-	var result=generate_multihex(2.0, [-179,179,-89,89]);
+function test_hex() {
+	"use strict";
+	var result = generate_multihex(1, [-179.0, 179.0, -89.0, 89.0]);
 	return result;
-	
 }
-
-
-
-
-
